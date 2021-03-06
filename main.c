5d2c7166e8d394b3a9483f88d6f7f7a4b9acc94c
igor0212 <igor_0212@hotmail.com>
2021-03-06 16:45:32 -0300

    Lendo arquivo

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ char *nome_algoritmo;
 char *nome_arquivo;
 int tamanho_paginas;
 int tamanho_memoria;
+int tamanho_tabela;
 
 void leitura(FILE *arquivo_log)
 {
@@ -29,6 +30,7 @@ void main( int argc, char *argv[] )  {
     nome_arquivo = argv[2];
     tamanho_paginas = atoi(argv[3]);
     tamanho_memoria = atoi(argv[4]);    
+    tamanho_tabela = tamanho_memoria/tamanho_paginas;
 
     if(strcmp(nome_algoritmo, "lru") != 0 && strcmp(nome_algoritmo, "2a") != 0 && strcmp(nome_algoritmo, "fifo") != 0)
     {
@@ -40,6 +42,7 @@ void main( int argc, char *argv[] )  {
     printf("Arquivo de entrada: %s\n", nome_arquivo);
     printf("Tamanho da memoria: %d KB\n", tamanho_memoria);
     printf("Tamanho das páginas: %d KB\n", tamanho_paginas);
+    printf("Tamanho da tabela: %d KB\n", tamanho_tabela);
     printf("Técnica de reposição: %s\n", nome_algoritmo);    
     
     FILE *arquivo_log = fopen(nome_arquivo, "r");
@@ -50,6 +53,12 @@ void main( int argc, char *argv[] )  {
     }
 
     leitura(arquivo_log);
+
+    fclose(arquivo_log);
     
     fim = clock();    
+
+    double tempo_execucao = (fim - inicio)/CLOCKS_PER_SEC;
+
+    printf("Tempo de execução: %f\n", tempo_execucao);
 };
