d7e918fb4be83b515418bf7f8016ac444b3edfa4
igor0212 <igor_0212@hotmail.com>
2021-03-07 21:26:51 -0300

    Correção fifo

--- doisa.h
+++ doisa.h
@@ -1,5 +1,5 @@
 #include "estruturas.h"
 
-Tabela doisa_execucao(int tamanho_tabela, int pagina_acesso,  unsigned int endereco, char operacao, int clock_cont);
+Tabela doisa_execucao(int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao, int clock_cont);
 
 void doisa_listagem(Tabela tabela, int tamanho_tabela);
