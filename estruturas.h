003e4898c5a681befa997f12d82387e735bbc11a
igor0212 <igor_0212@hotmail.com>
2021-03-08 20:58:32 -0300

    Refatorando

--- estruturas.h
+++ estruturas.h
@@ -4,29 +4,42 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+
 #pragma once
-typedef struct Pagina{
+
+typedef struct Pagina
+{
     int identificador;
     unsigned int numero;
     unsigned int ultimo_endereco_acessado;
     int ultimo_acesso;
     bool suja;    
 } Pagina;
-typedef struct Tabela{
+
+typedef struct StructItem
+{         
+    Pagina pagina;         
+    struct StructItem *proximo;       
+} Item;
+
+typedef struct Fila
+{         
+    Item *inicio;  
+    Item *fim; 
+    int tamanho;       
+} Fila;
+
+typedef struct Tabela
+{
     unsigned int num_entradas;
     Pagina *paginas;
 } Tabela;
-typedef struct Quadro{
+
+typedef struct Quadro
+{
     unsigned int ultimo_acesso;
     unsigned int _carregamento;
     bool esta_na_memoria;
 } Quadro;
-typedef struct StructItem{         
-    Pagina pagina;         
-    struct StructItem *proximo;       
-} Item;
-typedef struct ListaDetectada{         
-    Item *inicio;  Item *fim;  int tamanho;       
-} Fila;
 
 void adicionarItemFila(Fila *fila, Item *item, Pagina pagina);
