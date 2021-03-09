017f3ad67a49ebf0382d54360f21c971999683f0
igor0212 <igor_0212@hotmail.com>
2021-03-09 20:47:07 -0300

    Refatoracao 2a

--- segundachance.c
+++ segundachance.c
@@ -2,41 +2,9 @@
 
 int idx;
 
-Tabela segundachance_execucao(int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao, int clock_cont, int *paginas_lidas, int *paginas_escritas)
-{    
-    Tabela tabela;
-    tabela.paginas = (Pagina *) malloc(tamanho_tabela * sizeof(Pagina));    
-    for(idx = 0; idx < tamanho_tabela; idx++)
-    {
-        tabela.paginas[idx].id = -1;        
-        //tabela.paginas[idx].chance = false;
-    }
-
-    ItemMemoria itens_memoria[tamanho_tabela];
-    for(idx = 0; idx < tamanho_tabela; idx++)
-    {
-        itens_memoria[idx].existe = false;
-    }    
-  
-    int pagina_tabela = 0;
-    for(idx = 0; idx < tamanho_tabela; idx++)
-    {      
-      if(tabela.paginas[idx].numero == pagina_acesso)
-      {     
-        *paginas_escritas+=1;
-        tabela.paginas[idx].endereco_acessado = endereco;        
-        //tabela.paginas[idx].ultimo_acesso = clock_cont;        
-        tabela.paginas[idx].bit_controle = operacao == 'W';
-        //tabela.paginas[idx].chance = true;
-        pagina_tabela = 1;
-
-        break;
-      }
-    }
-    
-    if(pagina_tabela == 0)
-    {    
-      *paginas_lidas+=1;
+Tabela analisarPaginaForaTabela(Tabela tabela, ItemMemoria* itens_memoria, int *paginas_lidas, int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao)
+{
+      *paginas_lidas += 1;
       int idx_inserir = -1;
       int auxiliar_minimo = -1;
 
@@ -59,9 +27,8 @@ Tabela segundachance_execucao(int tamanho_tabela, int pagina_acesso, unsigned in
             idx_inserir = idx;
           }
 
-          if((idx == tamanho_tabela - 1))// && tabela.paginas[idx_inserir].chance)
-          {   
-              //tabela.paginas[idx_inserir].chance = false;           
+          if((idx == tamanho_tabela - 1))
+          {                 
               idx_inserir = 0;               
           }
         }
@@ -73,7 +40,43 @@ Tabela segundachance_execucao(int tamanho_tabela, int pagina_acesso, unsigned in
       tabela.paginas[idx_inserir].endereco_acessado = endereco;        
 
       return tabela;
+}
+
+Tabela segundachance_execucao(int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao, int clock_cont, int *paginas_lidas, int *paginas_escritas)
+{    
+    Tabela tabela;
+    tabela.paginas = (Pagina *) malloc(tamanho_tabela * sizeof(Pagina));    
+    for(idx = 0; idx < tamanho_tabela; idx++)
+    {
+        tabela.paginas[idx].id = -1;                
     }
+
+    ItemMemoria itens_memoria[tamanho_tabela];
+    for(idx = 0; idx < tamanho_tabela; idx++)
+    {
+        itens_memoria[idx].existe = false;
+    }    
+  
+    int pagina_tabela = 0;
+    for(idx = 0; idx < tamanho_tabela; idx++)
+    {      
+      if(tabela.paginas[idx].numero == pagina_acesso)
+      {     
+        *paginas_escritas+=1;
+        tabela.paginas[idx].endereco_acessado = endereco;                    
+        tabela.paginas[idx].bit_controle = operacao == 'W';        
+        pagina_tabela = 1;
+
+        break;
+      }
+    }    
+    
+    if(pagina_tabela == 0)
+    {    
+      return analisarPaginaForaTabela(tabela, itens_memoria, paginas_lidas, tamanho_tabela, pagina_acesso, endereco, operacao);
+    }
+
+    return tabela;
 }
 
 void segundachance_listagem(Tabela tabela, int tamanho_tabela)
--- segundachance.h
+++ segundachance.h
@@ -1,5 +1,7 @@
 #include "estruturas.h"
 
+Tabela analisarPaginaForaTabela(Tabela tabela, ItemMemoria* itens_memoria, int *paginas_lidas, int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao);
+
 Tabela segundachance_execucao(int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao, int clock_cont, int *paginas_lidas, int *paginas_escritas);
 
 void segundachance_listagem(Tabela tabela, int tamanho_tabela);
