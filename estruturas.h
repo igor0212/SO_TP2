4a18a3d5697597a132cec12e9984ced6f4a0518d
igor0212 <igor_0212@hotmail.com>
2021-03-08 21:28:34 -0300

    estrutura funcionando

--- estruturas.h
+++ estruturas.h
@@ -1,32 +1,42 @@
-#include <time.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <stdbool.h>
+
 #pragma once
-typedef struct Pagina{
+
+typedef struct Pagina
+{
     int quadro;
     unsigned int numero;
     unsigned int ultimo_endereco_acessado;
     int ultimo_acesso;
     bool suja;    
 } Pagina;
-typedef struct Tabela{
+
+typedef struct StructItem
+{         
+    Pagina pagina;         
+    struct StructItem *proximo;       
+} Item;
+
+typedef struct Fila
+{         
+    Item *inicio;
+    Item *fim;
+    int tamanho;       
+} Fila;
+
+typedef struct Tabela
+{
     unsigned int num_entradas;
     Pagina *paginas;
 } Tabela;
-typedef struct Quadro{
+
+typedef struct Quadro
+{
     unsigned int ultimo_acesso;
     unsigned int _carregamento;
     bool esta_na_memoria;
 } Quadro;
-typedef struct StructItem{         
-    Pagina pagina;         
-    struct StructItem *proximo;       
-} Item;
-typedef struct ListaDetectada{         
-    Item *inicio;  Item *fim;  int tamanho;       
-} Fila;
 
 void adicionarItemFila(Fila *fila, Item *item, Pagina pagina);
