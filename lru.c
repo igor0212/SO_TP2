3c53997685f24b06e0c2d75f346b2bd56b52486e
igor0212 <igor_0212@hotmail.com>
2021-03-08 22:04:12 -0300

    fat

--- lru.c
+++ lru.c
@@ -2,7 +2,7 @@
 
 int idx;
 
-Tabela lru_execucao(int tamanho_tabela, int numero_pagina_acessada,  unsigned int endereco, char operacao, int contador_clock, Quadro *quadros_memoria, int *paginas_lidas, int *paginas_escritas) 
+Tabela lru_execucao(int tamanho_tabela, int pagina_acesso, unsigned int endereco, char operacao, int contador_clock, Quadro *quadros_memoria, int *paginas_lidas, int *paginas_escritas) 
 {
       Tabela tabela;
       tabela.paginas = (Pagina *) malloc(tamanho_tabela * sizeof(Pagina));    
@@ -11,40 +11,46 @@ Tabela lru_execucao(int tamanho_tabela, int numero_pagina_acessada,  unsigned in
           tabela.paginas[idx].id = -1;        
       }          
 
-      int pagina_esta_na_tabela = 0;
-      for(idx = 0; idx < tamanho_tabela; idx++){
-        if(tabela.paginas[idx].numero == numero_pagina_acessada){    
-          *paginas_escritas += 1;      
-
+      int pagina_tabela = 0;
+      for(idx = 0; idx < tamanho_tabela; idx++)
+      {
+        if(tabela.paginas[idx].numero == pagina_acesso)
+        {    
+          *paginas_escritas += 1;
           tabela.paginas[idx].endereco_acessado = endereco;
           tabela.paginas[idx].bit_controle = (operacao == 'W');
           tabela.paginas[idx].ultimo_acesso = contador_clock;
-
-          pagina_esta_na_tabela = 1;
+          pagina_tabela = 1;
           break;
         }
       }
 
-      if(!pagina_esta_na_tabela){
+      if(pagina_tabela == 0)
+      {
         *paginas_lidas += 1;      
         int indice_quadro_a_inserir = -1;
-        for(idx = 0; idx < tamanho_tabela; idx++){
-          if(!quadros_memoria[idx].esta_na_memoria){
+        for(idx = 0; idx < tamanho_tabela; idx++)
+        {
+          if(!quadros_memoria[idx].esta_na_memoria)
+          {
             indice_quadro_a_inserir = idx;
             break;
           }
         }
-        if(indice_quadro_a_inserir == -1){
+        if(indice_quadro_a_inserir == -1)
+        {
           int menor_ultimo_acesso = -1;
-          for(idx = 0; idx < tamanho_tabela; idx++){
-            if(tabela.paginas[idx].ultimo_acesso < menor_ultimo_acesso){
+          for(idx = 0; idx < tamanho_tabela; idx++)
+          {
+            if(tabela.paginas[idx].ultimo_acesso < menor_ultimo_acesso)
+            {
               menor_ultimo_acesso = tabela.paginas[idx].ultimo_acesso;
               indice_quadro_a_inserir = idx;
             }
           }          
         }
 
-        tabela.paginas[indice_quadro_a_inserir].numero = numero_pagina_acessada;
+        tabela.paginas[indice_quadro_a_inserir].numero = pagina_acesso;
         tabela.paginas[indice_quadro_a_inserir].id = indice_quadro_a_inserir;
         tabela.paginas[indice_quadro_a_inserir].bit_controle = (operacao == 'W');
         tabela.paginas[indice_quadro_a_inserir].endereco_acessado = endereco;
